583205b9c3621becc5f41c7a03f5c9f189d3e6b0
simon <simon@desktop-2pb07dc.home>
2020-06-25 10:00:21 +0200

    test

--- src/Components/Schedule/index.js
+++ src/Components/Schedule/index.js
@@ -4,39 +4,66 @@ import React, { useEffect, useState } from 'react';
 import * as firebase from 'firebase/app';
 
 import { Inject, ScheduleComponent, Day, Week, WorkWeek, Month, Agenda, eventsLoaded } from '@syncfusion/ej2-react-schedule';
+import { Button, Modal, Box } from '@material-ui/core';
+
+import Swal from 'sweetalert2';
+
 import './index.css';
 
 export default () => {
     const db = firebase.firestore();
-    const [data, setData] = useState([]);
-    const [newData, setNewData] = useState(false);
+    const [open, setOpen] = useState(false);
+    const data = [];
     let scheduleObj = null;
 
+    const form = (
+        `
+        <div style="display: flex; flex-direction: column">
+            <input type="text" placeholder="Subject"/>
+            <input type="text" placeholder="Start: YYYY|M|dd|hh|mm"/>
+            <input type="text" placeholder="End"/>
+
+        </div>
+        `
+    );
+    
+    const modal = Swal.mixin({
+        title: "Make a new event",
+        html: form,
+        showCloseButton: true,
+        showConfirmButton: false
+    })
+
     const getUserSchedule = async () => {
-        const users = await db.collection('users').get();
+        const user = await db.collection('users').doc("W2Alg6PRUY5Ljhljrtim").get();
 
-        const calendarData = users.docs
-        .map(user => user.data())
-        .filter(user => user.calendarData != null)
-        .map(userWithCalendar => ({
-            Subject: userWithCalendar.calendarData[0].Subject,
-            StartTime: new Date(...userWithCalendar.calendarData[0].StartTime.split('|')),
-            EndTime: new Date(...userWithCalendar.calendarData[0].EndTime.split('|')),
-        }));
+        const calendarData = user.data().calendarData;
 
-        return calendarData;
+        return (
+            calendarData.map(data => ({
+                    Subject: (data.Subject || "No title"),
+                    StartTime: new Date(...data.StartTime.split('|')),
+                    EndTime: new Date(...data.EndTime.split('|'))
+                })
+            )
+        );
     };
 
     useEffect(() => {
-        if(typeof scheduleObj === 'object') {
-            const userData = getUserSchedule();
-            userData.then(userData => setData(userData));
-        }
+        const userData = getUserSchedule();
+        userData.then(userData => scheduleObj.addEvent(userData));
     }, [scheduleObj]);
 
     return (
-        <ScheduleComponent ref={t => scheduleObj = t} selectedDate={new Date(2020, 6)} eventSettings={{ dataSource: data }} currentView="Month">
-            <Inject services={[Day, Week, WorkWeek, Month, Agenda]} />
-        </ScheduleComponent>
+        <div>
+            <Box my={1} mx={1}>
+                <Button variant="contained" color="primary" onClick={() => modal.fire()}>
+                    Add event
+                </Button>
+            </Box>
+            <ScheduleComponent ref={t => scheduleObj = t} selectedDate={new Date(2020,6,25)} eventSettings={{ dataSource: data }} currentView="Week">
+                <Inject services={[Day, Week, WorkWeek, Month, Agenda]} />
+            </ScheduleComponent>
+        </div>
     );   
 }
